501bc8933db671eccc7636ef175c318a175c2ca4
siddharth-stn <66431369+siddharth-stn@users.noreply.github.com>
2021-04-23 16:08:37 +0530

    style loader and css loader dependencies added

---
+++ src/game.js
@@ -0,0 +1,40 @@
+import humanPlayer from "./humanPlayer.js";
+import aI_Player from "./computerPlayer";
+
+const humanName = prompt("Please Enter Your Name", "Human");
+const body = document.getElementsByTagName("body")[0];
+const turnDisp = document.getElementById("turnDispPara");
+
+let boardWrapperDiv = document.createElement("div");
+boardWrapperDiv.id = "boardWrapperDiv";
+boardWrapperDiv.className = "wrapperDiv";
+for (let y = 0; y < 10; y++) {
+  for (let x = 0; x < 10; x++) {
+    let childDiv = document.createElement("div");
+    childDiv.className = "cellDiv";
+    childDiv.id = `${x},${y}`;
+    childDiv.innerText = `div${x},${y}`;
+    boardWrapperDiv.appendChild(childDiv);
+  };
+};
+body.appendChild(boardWrapperDiv);
+
+
+const startBtn = document.getElementById("startBtn");
+
+// startBtn.addEventListener("click", (event) => {
+//   const playerOne = humanPlayer(humanName);
+//   const playerTwo_comp = aI_Player();
+//   let on = true;
+//   let turn = 1;
+//   while (on) {
+//     if (turn === 1) {
+//       turnDisp.innerText = `${playerOne.name}'s turn`;
+//     } else if (turn === 2) {
+//       turnDisp.innerText = `${playerTwo_comp.name}'s turn`;
+//     }
+
+//     turn = 2;
+//     on = false;
+//   }
+// });
